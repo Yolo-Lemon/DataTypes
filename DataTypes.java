54127ab1940c25085c3ce9f7bb544016b3b984e1
Yolo-Lemon <sveyzman@gmail.com>
2025-01-04 02:41:20 +0300

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,12 @@
+package datatypes;
+
+public class DataTypes {
+    public static void main(String args[]) {
+        double p = 1;
+        double pi = 3.14;
+        double r = p / (2 * pi);
+        double area = pi * (r * r);
+        System.out.println("Площадь круга равна " + area);
+
+    }
+}
