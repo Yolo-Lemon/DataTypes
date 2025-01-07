c987d6f5af5005f3d670edc3a03763e7a6530221
Yolo-Lemon <sveyzman@gmail.com>
2025-01-07 16:19:47 +0300

    new tasks

--- src/datetyp/DataTypes.java
+++ src/datetyp/DataTypes.java
@@ -7,6 +7,8 @@ public class DataTypes {
         task3();
         task4();
         task5();
+        task6();
+        task7();
     }
 
     //Дана длина окружности. Найти площадь круга, ограниченного этой окружностью. В качестве значения Pi использовать 3.14
@@ -29,9 +31,9 @@ public class DataTypes {
 
     //В кодировке КОИ-8 каждый символ кодируется одним байтом. Определите в битах информационный объем сообщения из 30 символов в этой кодировке.
     private static void task3() {
-        byte symbols = 30;
-        int bites = symbols * 8;
-        System.out.println("В кодировке КОИ-8 30 символов займут " + bites + " байтов");
+        int symbols = 30;
+        int bytes = symbols * 8;
+        System.out.println("В кодировке КОИ-8 30 символов займут " + bytes + " битов");
     }
 
     //В методе задачи вызови метод div(<аргумент: тип название_аргумента>) 2 раза. 
@@ -41,13 +43,28 @@ public class DataTypes {
         div(-1, 0);
     }
 
-    private static void div(float a, float b) {
+    private static void div(double a, double b) {
         System.out.println(a / b);
     }
 
+    //В методе main вызови метод div() с таким аргументом, чтобы вывод в консоли был "NaN". Код метода div() изменять нельзя.
     private static void task5() {
-        div(infinity, infinity);
+        double c = 1.0 / 0.0;
+        div(c, c);
+    }
 
+    private static void task6() {
+        int num1 = 7;
+        int num2 = 13;
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+        System.out.println("Число1 = " + num1 + " Число2 = " + num2);
+    }
 
+    private static void task7() {
+        char symbol1 = 'C';
+        char symbol2 = 1;
+        System.out.println(symbol1 + symbol2);
     }
 }
